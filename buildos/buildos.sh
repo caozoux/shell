18bfc6a95db4c7994a4952954a4ad1905303ddf6
zou cao <zoucaox@outlook.com>
2022-08-04 22:24:16 +0800

    update

--- buildos/buildos.sh
+++ buildos/buildos.sh
@@ -3,6 +3,8 @@
 curdir=`pwd`
 myspace=$curdir
 buildos_root=$curdir/buildos
+CENTOSVER=""
+OPENEULERVER=""
 
 function dockerimages_ssh()
 {
@@ -52,28 +54,100 @@ function centos_env_inst()
 	cp cfg/ifcfg-eth0    $buildos_root/etc/sysconfig/network-scripts/
 	cp cfg/fstab         $buildos_root/etc/
 }
+__ScriptVersion="1.0"
+
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+	echo "Usage :  $0 [options] [--]
+
+    Options:
+    -h|help       Display this message
+    -v|version    Display script version"
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+while getopts ":hvco" opt
+do
+  case $opt in
+
+	h|help     )  usage; exit 0   ;;
+
+	v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
+
+	c|centos_version ) 
+		CENTOSVER=$OPTAGR
+		;;
+	o|openeuler_version ) 
+		OPENEULERVER=$OPTAGR
+		;;
+
+	* )  echo -e "\n  Option does not exist : $OPTARG\n"
+		  usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $(($OPTIND-1))
 
 umount $buildos_root/dev
 umount $buildos_root/proc
 rm -rf  $buildos_root     > /dev/null
+rm -f  *.rpm > /dev/null
+
 [[ -d $buildos_root ]] && rm -fr $buildos_root && mkdir -p $buildos_root
 
-rpm --root $buildos_root --initdb
-yumdownloader centos-release
+function centosbuild() {
+	rpm --root $buildos_root --initdb
+	wget https://vault.centos.org/centos/8.2.2004/BaseOS/x86_64/os/Packages/centos-release-8.2-2.2004.0.1.el8.x86_64.rpm
+	wget https://vault.centos.org/centos/8.2.2004/BaseOS/x86_64/os/Packages/centos-gpg-keys-8.2-2.2004.0.1.el8.noarch.rpm
+	wget https://vault.centos.org/centos/8.2.2004/BaseOS/x86_64/os/Packages/centos-repos-8.2-2.2004.0.1.el8.x86_64.rpm
+	#wget https://vault.centos.org/centos/8.2.2004/BaseOS/x86_64/os/Packages/centos-gpg-keys-8.2-1.2004.0.2.el8.noarch.rpm
+	#wget https://vault.centos.org/centos/8.2.2004/BaseOS/x86_64/os/Packages/centos-repos-8.2-1.2004.0.2.el8.x86_64.rpm
+	#yumdownloader centos-release
 
-if [ $? -ne 0 ]
-then
-	echo "Error: download centos-release failed"
-	exit 1
-fi
+	if [ $? -ne 0 ]
+	then
+		echo "Error: download centos-release failed"
+		exit 1
+	fi
 
-rpm --root $buildos_root -ivh centos-release*
+	rpm --root $buildos_root -ivh centos-release* centos-gpg-keys* centos-repos*
+	rm *.rpm -f
+}
+
+function openeulerbuild() {
+	rpm --root $buildos_root --initdb
+	wget https://repo.openeuler.org/openEuler-22.03-LTS/OS/x86_64/Packages/openEuler-release-22.03LTS-52.oe2203.x86_64.rpm
+	wget https://repo.openeuler.org/openEuler-22.03-LTS/OS/x86_64/Packages/openEuler-repos-1.0-3.4.oe2203.x86_64.rpm
+	wget https://repo.openeuler.org/openEuler-22.03-LTS/OS/x86_64/Packages/openEuler-gpg-keys-1.0-3.4.oe2203.x86_64.rpm
+
+	if [ $? -ne 0 ]
+	then
+		echo "Error: download centos-release failed"
+		exit 1
+	fi
+
+	rpm --root $buildos_root -ivh *.rpm --nodepp
+	rm *.rpm
+}
+
+openeulerbuild
 #install yum without docs and install only english language files during the process：
 yum -y --installroot=$buildos_root --setopt=tsflags='nodocs' --setopt=override_install_langs=en_US.utf8 install yum yum-plugin-bestyumcache 
+
 #configure yum to avoid installing of docs and other language files than english generally
 sed -i "/installonly_limit=5/a override_install_langs=en_US.utf8\ntsflags=nodocs" $buildos_root/etc/yum.conf
+
 #chroot to the environment and install some additional tools：
 cp /etc/resolv.conf $buildos_root/etc
+
 #cp /etc/bashrc $buildos_root/root/.bashrc
 cp /etc/motd $buildos_root/etc/motd
 
